a1f8056fad99d9ee2ef411e0ab31463394010bdf
seonmin12 <seonmink12@naver.com>
2025-03-04 18:24:36 +0900

    BoardSeonmin

---
+++ BoardTeamPractice/VO/Board.java
@@ -0,0 +1,15 @@
+package BoardTeamPractice.VO;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Board {
+
+    private int bno;
+    private String btitle;
+    private String bcontent;
+    private String bwriter;
+    private Date bdate;
+}
