338a6fdd50f7a6e24bff06ecacf665e3c84f8a9f
seonmin12 <seonmink12@naver.com>
2025-03-04 19:06:35 +0900

    ignoreagain

--- boards/controller/BoardMain.java
+++
@@ -1,9 +0,0 @@
-package BoardTeamPractice.controller;
-
-public class BoardMain {
-    public static void main(String[] args) {
-        BoardMenu boardMenu = new BoardMenu();
-        boardMenu.list();
-
-    }
-}
